5f46d5b364dd37e1ee179a71622283184105ee5c
hr4programador <hr4programador@outlook.com>
2017-04-14 22:56:49 -0300

    Primeira Versão do MeuPrimeiroSite

---
+++ MeuPrimeiroSite/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeuPrimeiroSite.Controllers
+{
+    public class UsuarioController : Controller
+    {
+    }
+}

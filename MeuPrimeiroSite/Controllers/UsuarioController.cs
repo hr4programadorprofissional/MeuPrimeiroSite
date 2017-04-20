fdc2fd233950d5e7d818de96355414cd5cd753f5
hr4programador <hr4programador@outlook.com>
2017-04-19 22:35:53 -0300

    alterações usuariocontroller

--- MeuPrimeiroSite/Controllers/UsuarioController.cs
+++ MeuPrimeiroSite/Controllers/UsuarioController.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MeuPrimeiroSite.Controllers
 {
     public class UsuarioController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ObterMaioridade(string nome, string idade)
+        {
+            if (int.Parse(idade) < 18)
+            {
+                ViewBag.Mensagem = "Usuario: " + nome + " É menor de idade";
+            }
+            else
+            {
+                ViewBag.Mensagem = "Usuario: " + nome + " É maior de idade";
+            }
+
+            return View("Index");
+        }
     }
 }
